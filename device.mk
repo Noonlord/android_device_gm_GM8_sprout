305597b32260033bdbadaccc642a94ec51c0e153
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    GM8_sprout: Build 1.1 QTI power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- device.mk
+++ device.mk
@@ -126,6 +126,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.1-service-qti
+
 # Recovery Ramdisk
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
