2cd8084a61aa7859ffd413bc4f48f36a8465cd47
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    GM8_sprout: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- device.mk
+++ device.mk
@@ -131,6 +131,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
