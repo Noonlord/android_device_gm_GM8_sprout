c337b5d6f925bb66c24bed9b835b45a7643c9740
Akmal <magchuz@pm.me>
2020-07-06 17:05:32 +0700

    GM8_sprout: Enable power-libperfmgr
    Change-Id: Ibb3b29e58baeb984f4dc1bc02aab0fb8ad85c467

--- device.mk
+++ device.mk
@@ -218,6 +218,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3-service.gm-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Recovery Ramdisk
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
