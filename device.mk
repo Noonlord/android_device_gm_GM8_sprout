82863491d41e03b0cd0e4944e0ab181688f24400
Arian <arian.kulmer@web.de>
2020-09-22 17:32:07 +0200

    GM8_sprout: Add a shim for dpmframework
    * libdpmframework depends on strndup16to8 and strdup8to16 methods.
    
    Change-Id: I39d252b5830b752728a0c8607d32d8a279524ce1
    Co-authored-by: Demon000 <demonsingur@gmail.com>

--- device.mk
+++ device.mk
@@ -89,6 +89,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.1.vendor \
     vendor.display.config@1.2.vendor
 
+# DPM
+PRODUCT_PACKAGES += \
+    libshim_dpmframework
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
