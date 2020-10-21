296583e2c9935bb7812ae45f95421b55e8ddf942
Bruno Martins <bgcngm@gmail.com>
2020-10-21 17:01:46 +0100

    GM8_sprout: Patch camera HAL for Q bringup
    Camera HAL includes a check for "service.bootanim.exit" property to
    discern boot status and avoid notifying display HAL that a camera
    session is active during the bootup phase. Turns out that now, with
    a mix of old camera HAL and new display service, the display API
    doesn't seem to be called properly leaving camera HAL in a bad state.
    
    Specifically, on v20 the `service.bootanim.exit=1` path would trigger
    the stack protector when attempting to signal to display.qservice via
    sendSingleParam()
    
    Just skip notifying display HAL by fooling the camera HAL into believing
    that bootup phase never finished.
    
    Change-Id: I2a12b5373e2ea851fa9d678c3cee9dab9daf25f8

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+    vendor/lib/hw/camera.msm8937.so)
+        sed -i "s/service.bootanim.exit/service.bootanim.zzzz/g" "${2}"
+        ;;
     esac
 }
 
