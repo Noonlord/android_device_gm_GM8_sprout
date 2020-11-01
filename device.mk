10f9cda2d3cfa6c9d540604c52fd876fa698cf2b
Akash Mondal <a98mondal@gmail.com>
2020-11-01 22:22:18 +0000

    GM8_sprout: Build power stats HAL
    Change-Id: I7e6ad2e66e2dde79ead927daafe01bd83bf3e479

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.gm-libperfmgr
+    android.hardware.power@1.3-service.gm-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
