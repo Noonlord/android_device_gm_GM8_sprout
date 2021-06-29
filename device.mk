0d5b069d37604a1eb01c77adfff3e7f6745837f2
Noonlord <mymmym22@gmail.com>
2021-06-29 03:57:35 +0300

    GM8_sprout: Build AOSP camera stack from source

--- device.mk
+++ device.mk
@@ -14,6 +14,14 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += otapreopt_script
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
+    camera.device@3.4-external-impl \
+    camera.device@3.4-impl
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8937 \
