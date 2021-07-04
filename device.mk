beb4461abc1a72b500d7d4103887e0577f28e1ed
jonglin_lee <>
2021-07-04 18:25:35 +0300

    GM8_sprout: Symlink bootdevice in recovery Bug: 28639949 Bug: 28433543
    Change-Id: I17a351601408aefd0bcd5fc5ebde05fb55363fc2

--- device.mk
+++ device.mk
@@ -122,6 +122,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Recovery Ramdisk
+PRODUCT_PACKAGES += \
+    init.recovery.qcom.rc
+
 # Soundtrigger
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.0-core\
