16e41f3c1a1fd366a3f98b7e01a3c179bf37e22f
Noonlord <mymmym22@gmail.com>
2021-07-07 15:02:22 +0300

    GM8_sprout: Make DT2W work with two panels

--- power/power-feature.c
+++ power/power-feature.c
@@ -13,11 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "power-feature.h"
 
+#include <hardware/power.h>
 #include <log/log.h>
 
-void set_device_specific_feature(feature_t feature, int state) {
-    // stub
+#define TAP_TO_WAKE_HIMAX "/proc/android_touch/SMWP"
+#define TAP_TO_WAKE_FTS "/sys/android_touch/wakeup_gesture_enable"
+
+void set_device_specific_feature(struct power_module *module __unused,
+    feature_t feature, int state)
+{
+    if (feature == POWER_FEATURE_DOUBLE_TAP_TO_WAKE) {
+        char value = state ? '1' : '0';
+        int fd = open(TAP_TO_WAKE_FTS, O_WRONLY);
+        if (fd < 0){
+            ALOGD("Opening TAP_TO_WAKE_FTS failed, trying HIMAX");
+            fd = open(TAP_TO_WAKE_HIMAX, O_WRONLY);
+            if (fd < 0){
+                ALOGE("Opening TAP_TO_WAKE_HIMAX failed!");                }
+            else
+                ALOGD("Success");
+        }
+        int len = write(fd, &value, sizeof(value));
+        if (len < 0){
+            ALOGE("Writing failed!");
+        }
+    }
 }
